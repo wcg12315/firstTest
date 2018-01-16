51f28bb7a155e87143e2d440dfb645726eecf72b
wcg12315 <652929396@qq.com>
2018-01-16 22:04:36 +0800

    Demo代码提交，后续进行分组开发

---
+++ photoPK/pages/imgShow/imgShow.js
@@ -0,0 +1,44 @@
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    src: "../image/pic4.jpg"
+  },
+  gotoShow: function () {
+    var _this = this
+    wx.chooseImage({
+      count: 9, // 最多可以选择的图片张数，默认9
+      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+      success: function (res) {
+        // success
+        console.log(res)
+        _this.setData({
+          src: res.tempFilePaths
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  imageLoad: function (e) {
+    var $width = e.detail.width, //获取图片真实宽度
+      $height = e.detail.height,
+      ratio = $width / $height; //图片的真实宽高比例
+    var viewWidth = 718, //设置图片显示宽度，左右留有16rpx边距
+      viewHeight = 718 / ratio; //计算的高度值
+    var image = this.data.images;
+    //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
+    image[e.target.dataset.index
+    ] = {
+        width: viewWidth,
+        height: viewHeight
+      }
+    this.setData({
+      images: image
+    })
+  }
+})
--- photoPK/pages/index/index.js
+++ photoPK/pages/index/index.js
@@ -5,14 +5,21 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    motto1: 'Hello World1',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  //事件处理函数
+  gotoImgDemo: function () {
+    wx.navigateTo({
+      url: '/imgShow/imgShow'
     })
   },
   onLoad: function () {
@@ -21,7 +28,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,12 +50,19 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '头像PK',
+      desc: '好友头像大比拼，谁是最佳!',
+      path: '/index/index'
+    }
   }
 })
